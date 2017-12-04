a7a6326a75e0cc4437c45896735e5bcfd8b06e3a
dvanhise <dvanhise@gmail.com>
2017-12-03 20:30:14 -0500

    'commit'

---
+++ main.py
@@ -0,0 +1,31 @@
+import re
+from sympy.ntheory.generate import nextprime
+
+
+PROTOTYPE_FILE = './example.txt'
+OUTPUT_FILE = './output.txt'
+
+
+def main():
+    lineLength, baseNum = getNumFromFile(PROTOTYPE_FILE)
+    prime = nextprime(baseNum)
+    printNumToFile(OUTPUT_FILE, prime, lineLength)
+
+
+def getNumFromFile(filename):
+    with open(filename, 'r') as numfile:
+        length = len(re.sub(r'[^0-9]', '', numfile.readline()))
+        numfile.seek(0)
+        numString = re.sub(r'[^0-9]', '', numfile.read())
+    return length, int(numString)
+
+
+def printNumToFile(filename, num, lineLength):
+    output = str(num)
+    with open(filename, 'w') as outfile:
+        for line in range(len(output) // lineLength):
+            outfile.write(output[line*lineLength:(line+1)*lineLength] + '\n')
+
+
+if __name__ == "__main__":
+    main()
